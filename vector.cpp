e3b4cdc94c08b75b4b0d9e5fa40c25aa7a2a3c92
Anton <azoto033@gmail.com>
2024-08-13 21:23:52 +0300

    Initial commit

---
+++ vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Fin on 13.08.2024.
+//
+
+#include "vector.h"
---
+++ vector.h
@@ -0,0 +1,18 @@
+//
+// Created by Fin on 13.08.2024.
+//
+
+#ifndef VECTOR_VECTOR_H
+#define VECTOR_VECTOR_H
+
+template<typename T>
+class vector {
+    vector();
+    vector(int size, T value);
+    vector(const vector<T> &other);
+
+
+};
+
+
+#endif //VECTOR_VECTOR_H
