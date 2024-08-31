c958c327e513bc8e77af981bd51dc45790c4110e
Anton <azoto033@gmail.com>
2024-08-31 17:16:10 +0300

    Added new methods to the vector class: at(size_t index) const at(size_t index) const front() const front() const back() const back() const pop_back() pop_front() resize(size_t new_size) resize(size_t new_size, const T& value) shrink_to_fit() Implemented the iterator class within the vector class. Added begin(), end(), cbegin(), and cend() methods for iterator support. Ensured exception safety and proper resource management using RAII principles.

--- vector.cpp
+++ vector.cpp
@@ -4,90 +4,268 @@
 
 #include "vector.h"
 #include <algorithm>
+#include <stdexcept>
 
 namespace my_vector {
 
     template<typename T>
-    vector<T>::vector() : size(0), capacity(0), data(nullptr) {
+    vector<T>::vector() : size_(0), capacity_(0), data_(nullptr) {
     }
 
     template<typename T>
-    vector<T>::vector(int size, T value) : size(size), capacity(size), data(allocator.allocate(size)){
+    vector<T>::vector(size_t size, T value) : size_(size), capacity_(size), data_(allocator.allocate(size)){
       for (int i = 0; i < size; i++) {
-        allocator.construct(&this->data[i], value);
+        allocator.construct(&this->data_[i], value);
       }
     }
 
     template<typename T>
-    vector<T>::vector(const vector<T>& other) : size(other.size), capacity(other.capacity), data(allocator.allocate(other.capacity)) {
-      for (int i = 0; i < other.size; ++i) {
-        allocator.construct(&this->data[i], other.data[i]);
+    vector<T>::vector(const vector<T>& other) : size_(other.size_), capacity_(other.capacity_), data_(allocator.allocate(other.capacity_)) {
+      for (int i = 0; i < other.size_; ++i) {
+        allocator.construct(&this->data_[i], other.data_[i]);
       }
     }
 
     template<typename T>
     vector<T>& vector<T>::operator=(const vector<T>& other) {
       if (this != &other) {
-        for (int i = 0; i < size; ++i) {
-          allocator.destroy(&data[i]);
+        for (int i = 0; i < size_; ++i) {
+          allocator.destroy(&data_[i]);
         }
-        allocator.deallocate(data, capacity);
+        allocator.deallocate(data_, capacity_);
 
-        this->size = other.size;
-        this->capacity = other.capacity;
-        this->data = allocator.allocate(other.capacity);
-        for (int i = 0; i < other.size; ++i) {
-          allocator.construct(&this->data[i], other.data[i]);
+        this->size_ = other.size_;
+        this->capacity_ = other.capacity_;
+        this->data_ = allocator.allocate(other.capacity_);
+        for (int i = 0; i < other.size_; ++i) {
+          allocator.construct(&this->data_[i], other.data_[i]);
         }
       }
       return *this;
     }
 
     template<typename T>
-    vector<T>::vector(vector<T>&& other) noexcept : size(other.size), capacity(other.capacity), data(other.data) {
-      other.size = 0;
-      other.capacity = 0;
-      other.data = nullptr;
+    constexpr T& vector<T>::operator[](size_t index) const {
+      return data_[index];
+    }
+
+    template<typename T>
+    vector<T>::vector(vector<T>&& other) noexcept : size_(other.size_), capacity_(other.capacity_), data_(other.data_) {
+      other.size_ = 0;
+      other.capacity_ = 0;
+      other.data_ = nullptr;
     }
 
     template<typename T>
     vector<T>& vector<T>::operator=(vector<T>&& other) noexcept {
       if(this != &other){
-        for(int i = 0; i < size; ++i){
-          allocator.destroy(&data[i]);
+        for(int i = 0; i < size_; ++i){
+          allocator.destroy(&data_[i]);
         }
-        allocator.deallocate(data, capacity);
+        allocator.deallocate(data_, capacity_);
 
-        this->size = other.size;
-        this->capacity = other.capacity;
-        this->data = other.data;
-        other.size = 0;
-        other.capacity = 0;
-        other.data = nullptr;
+        this->size_ = other.size_;
+        this->capacity_ = other.capacity_;
+        this->data_ = other.data_;
+        other.size_ = 0;
+        other.capacity_ = 0;
+        other.data_ = nullptr;
       }
       return *this;
     }
 
     template<typename T>
     vector<T>::~vector() {
-      for (int i = 0; i < size; ++i) {
-        allocator.destroy(&data[i]);
+      for (int i = 0; i < size_; ++i) {
+        allocator.destroy(&data_[i]);
       }
-      allocator.deallocate(data, capacity);
+      allocator.deallocate(data_, capacity_);
     }
 
     template<typename T>
-    void vector<T>::recap(int new_capacity) {
-      if(new_capacity > capacity){
+    bool vector<T>::is_empty() noexcept {
+      return size_ == 0;
+    }
+
+    template<typename T>
+    void vector<T>::reserve(size_t new_capacity) {
+      if(new_capacity > capacity_){
         T* new_data = allocator.allocate(new_capacity);
-        for(int i = 0; i < size; ++i){
-          allocator.construct(&new_data[i], std::move(data[i]));
-          allocator.deallocate(&data[i]);
+        for(int i = 0; i < size_; ++i){
+          allocator.construct(&new_data[i], std::move(data_[i]));
+          allocator.deallocate(&data_[i]);
         }
-        allocator.deallocate(data, capacity);
-        data = new_data;
-        capacity = new_capacity;
+        allocator.deallocate(data_, capacity_);
+        data_ = new_data;
+        capacity_ = new_capacity;
       }
     }
+
+    template<typename T>
+    void vector<T>::push_back(const T& value) {
+      if(size_ == capacity_){
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+      }
+      allocator.construct(&data_[size_++], value);
+    }
+
+    template<typename T>
+    void vector<T>::push_back(T&& value) {
+      if(size_ == capacity_){
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+      }
+      allocator.construct(&data_[size_++], std::move(value));
+    }
+
+    template<typename T>
+    void vector<T>::push_front(const T& value) {
+      if(size_ == capacity_){
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+      }
+      for(int i = size_; i > 0; --i){
+        data_[i] = std::move(data_[i - 1]);
+      }
+      allocator.construct(&data_[0], value);
+      size_++;
+    }
+
+    template<typename T>
+    void vector<T>::push_front(T&& value) {
+      if(size_ == capacity_){
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+      }
+      for(int i = size_; i > 0; --i){
+        data_[i] = std::move(data_[i - 1]);
+      }
+      allocator.construct(&data_[0], std::move(value));
+      size_++;
+    }
+
+    template<typename T>
+    void constexpr vector<T>::clear() noexcept {
+      for(int i = 0; i < size_; ++i){
+        allocator.destroy(&data_[i]);
+      }
+      size_ = 0;
+    }
+
+    template<typename T>
+    void vector<T>::swap(vector& other) noexcept {
+      std::swap(size_, other.size_);
+      std::swap(capacity_, other.capacity_);
+      std::swap(data_, other.data_);
+    }
+
+    template<typename T>
+    T& vector<T>::at(size_t index) {
+      if (index >= size_) {
+        throw std::out_of_range("Index out of range");
+      }
+      return data_[index];
+    }
+
+    template<typename T>
+    const T& vector<T>::at(size_t index) const {
+      if (index >= size_) {
+        throw std::out_of_range("Index out of range");
+      }
+      return data_[index];
+    }
+
+    template<typename T>
+    T& vector<T>::front() {
+      if (is_empty()) {
+        throw std::out_of_range("Vector is empty");
+      }
+      return data_[0];
+    }
+
+    template<typename T>
+    const T& vector<T>::front() const {
+      if (is_empty()) {
+        throw std::out_of_range("Vector is empty");
+      }
+      return data_[0];
+    }
+
+    template<typename T>
+    T& vector<T>::back() {
+      if (is_empty()) {
+        throw std::out_of_range("Vector is empty");
+      }
+      return data_[size_ - 1];
+    }
+
+    template<typename T>
+    const T& vector<T>::back() const {
+      if (is_empty()) {
+        throw std::out_of_range("Vector is empty");
+      }
+      return data_[size_ - 1];
+    }
+
+    template<typename T>
+    void vector<T>::pop_back() {
+      if (is_empty()) {
+        throw std::out_of_range("Vector is empty");
+      }
+      allocator.destroy(&data_[--size_]);
+    }
+
+    template<typename T>
+    void vector<T>::pop_front() {
+      if (is_empty()) {
+        throw std::out_of_range("Vector is empty");
+      }
+      allocator.destroy(&data_[0]);
+      for (size_t i = 1; i < size_; ++i) {
+        data_[i - 1] = std::move(data_[i]);
+      }
+      --size_;
+    }
+
+    template<typename T>
+    void vector<T>::resize(size_t new_size) {
+      if (new_size > capacity_) {
+        reserve(new_size);
+      }
+      for (size_t i = size_; i < new_size; ++i) {
+        allocator.construct(&data_[i]);
+      }
+      for (size_t i = new_size; i < size_; ++i) {
+        allocator.destroy(&data_[i]);
+      }
+      size_ = new_size;
+    }
+
+    template<typename T>
+    void vector<T>::resize(size_t new_size, const T& value) {
+      if (new_size > capacity_) {
+        reserve(new_size);
+      }
+      for (size_t i = size_; i < new_size; ++i) {
+        allocator.construct(&data_[i], value);
+      }
+      for (size_t i = new_size; i < size_; ++i) {
+        allocator.destroy(&data_[i]);
+      }
+      size_ = new_size;
+    }
+
+    template<typename T>
+    void vector<T>::shrink_to_fit() {
+      if (size_ < capacity_) {
+        T* new_data = allocator.allocate(size_);
+        for (size_t i = 0; i < size_; ++i) {
+          allocator.construct(&new_data[i], std::move(data_[i]));
+          allocator.destroy(&data_[i]);
+        }
+        allocator.deallocate(data_, capacity_);
+        data_ = new_data;
+        capacity_ = size_;
+      }
+    }
+
+
 
 } //namespace my_vector
--- vector.h
+++ vector.h
@@ -18,36 +18,61 @@ namespace my_vector {
  */
     template<typename T>
     class vector {
-        int size; /// Number of elements in the vector
-        int capacity; /// Allocated storage capacity of the vector
-        T* data; /// Pointer to the allocated storage
+
+        class iterator {
+            T* ptr;
+        public:
+            explicit iterator(T* ptr) : ptr(ptr) {}
+            iterator& operator++() { ++ptr; return *this; }
+            iterator operator++(int) { iterator tmp = *this; ++ptr; return tmp; }
+            T& operator*() { return *ptr; }
+            T* operator->() { return ptr; }
+            bool operator==(const iterator& other) const { return ptr == other.ptr; }
+            bool operator!=(const iterator& other) const { return ptr != other.ptr; }
+        };
+
+        iterator begin() { return iterator(data_); }
+        iterator end() { return iterator(data_ + size_); }
+        iterator cbegin() const { return iterator(data_); }
+        iterator cend() const { return iterator(data_ + size_); }
+
+        size_t size_; /// Number of elements in the vector
+        size_t capacity_; /// Allocated storage capacity_ of the vector
+        T* data_; /// Pointer to the allocated storage
         std::allocator<T> allocator; /// Allocator for managing memory
 
       /**
-       * @brief Resizes the vector to a new capacity.
+       * @brief Resizes the vector to a new capacity_.
        *
-       * If the current capacity is exceeded, reallocates storage with the new capacity.
+       * If the current capacity_ is exceeded, reallocates storage with the new capacity_.
        *
-       * @param new_capacity The new capacity of the vector.
+       * @param new_capacity The new capacity_ of the vector.
        */
-        void recap(int new_capacity);
+        void reserve(size_t new_capacity);
+
+      /**
+       * @brief Checks if the vector is empty.
+       *
+       * @return True if the vector is empty, false otherwise.
+       */
+        bool is_empty() noexcept;
     public:
         /**
          * @brief Default constructor.
          *
-         * Initializes the vector with size and capacity set to 0, and data set to nullptr.
+         * Initializes the vector with size_ and capacity_ set to 0, and data_ set to nullptr.
          */
         vector();
 
         /**
-         * @brief Constructor with size and value.
+         * @brief Constructor with size_ and value.
          *
-         * Initializes the vector with a given size and fills it with the specified value.
+         * Initializes the vector with a given size_ and fills it with the specified value.
          *
          * @param size The number of elements to initialize.
          * @param value The value to initialize each element with.
          */
-        vector(int size, T value);
+        vector(size_t size, T value);
 
         /**
          * @brief Copy constructor.
@@ -88,11 +113,181 @@ namespace my_vector {
         vector<T>& operator=(vector<T>&& other) noexcept;
 
         /**
+         * @brief Accesses the element at the specified position.
+         *
+         * @param index The position of the element to access.
+         * @return A reference to the element at the specified position.
+         */
+        constexpr T& operator[] (size_t index) const;
+
+        /**
          * @brief Destructor.
          *
          * Destroys the vector and deallocates its memory.
          */
         ~vector();
+
+        /**
+         * @brief Adds an element to the end of the vector.
+         *
+         * @param value The value to add.
+         */
+        void push_back(const T& value);
+
+        /**
+         * @brief Adds an element to the end of the vector using move semantics.
+         *
+         * @param value The value to add.
+         */
+        void push_back(T&& value);
+
+        /**
+         * @brief Adds an element to the front of the vector.
+         *
+         * @param value The value to add.
+         */
+        void push_front(const T& value);
+
+        /**
+         * @brief Adds an element to the front of the vector using move semantics.
+         *
+         * @param value The value to add.
+         */
+        void push_front(T&& value);
+
+        /**
+         * @brief Clears the contents of the vector.
+         *
+         * Sets the size_ to 0 but does not deallocate the memory.
+         */
+        void constexpr clear() noexcept;
+
+        /**
+         * @brief Swaps the contents of this vector with another vector.
+         *
+         * @param other The vector to swap with.
+         */
+        void swap(vector& other) noexcept;
+
+        /**
+         * @brief Returns the number of elements in the vector.
+         *
+         * @return The number of elements in the vector.
+         */
+        size_t size() const noexcept;
+
+        /**
+         * @brief Returns the capacity_ of the vector.
+         *
+         * @return The capacity_ of the vector.
+         */
+        size_t capacity() const noexcept;
+
+        /**
+         * @brief Accesses the element at the specified position.
+         *
+         * @param index The position of the element to access.
+         * @return A reference to the element at the specified position.
+         * @throws std::out_of_range if the index is out of range.
+         */
+        T& at(size_t index);
+
+        /**
+         * @brief Accesses the element at the specified position (const version).
+         *
+         * @param index The position of the element to access.
+         * @return A const reference to the element at the specified position.
+         * @throws std::out_of_range if the index is out of range.
+         */
+        const T& at(size_t index) const;
+
+        /**
+         * @brief Accesses the first element.
+         *
+         * @return A reference to the first element.
+         * @throws std::out_of_range if the vector is empty.
+         */
+        T& front();
+
+        /**
+         * @brief Accesses the first element (const version).
+         *
+         * @return A const reference to the first element.
+         * @throws std::out_of_range if the vector is empty.
+         */
+        const T& front() const;
+
+        /**
+         * @brief Accesses the last element.
+         *
+         * @return A reference to the last element.
+         * @throws std::out_of_range if the vector is empty.
+         */
+        T& back();
+
+        /**
+         * @brief Accesses the last element (const version).
+         *
+         * @return A const reference to the last element.
+         * @throws std::out_of_range if the vector is empty.
+         */
+        const T& back() const;
+
+        /**
+         * @brief Removes the last element of the vector.
+         *
+         * @throws std::out_of_range if the vector is empty.
+         */
+        void pop_back();
+
+        /**
+         * @brief Removes the first element of the vector.
+         *
+         * @throws std::out_of_range if the vector is empty.
+         */
+        void pop_front();
+
+        /**
+         * @brief Resizes the vector to contain the specified number of elements.
+         *
+         * If the new size_ is greater than the current size_, new elements are default-initialized.
+         * If the new size_ is less than the current size_, elements are destroyed.
+         *
+         * @param new_size The new size_ of the vector.
+         */
+        void resize(size_t new_size);
+
+        /**
+         * @brief Resizes the vector to contain the specified number of elements, initializing new elements with the specified value.
+         *
+         * If the new size_ is greater than the current size_, new elements are initialized with the specified value.
+         * If the new size_ is less than the current size_, elements are destroyed.
+         *
+         * @param new_size The new size_ of the vector.
+         * @param value The value to initialize new elements with.
+         */
+        void resize(size_t new_size, const T& value);
+
+        /**
+         * @brief Shrinks the capacity_ of the vector to fit its size_.
+         */
+        void shrink_to_fit();
+
+        /**
+         * @brief Returns a pointer to the underlying array.
+         *
+         * @return A pointer to the underlying array.
+         */
+        T* data() noexcept;
+
+        /**
+         * @brief Returns a const pointer to the underlying array.
+         *
+         * @return A const pointer to the underlying array.
+         */
+        const T* data() const noexcept;
+
+
 
         //TODO: vector<bool>, bit_reference, iterator(?)
     };
